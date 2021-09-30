d61f72fc2fe68c10cbaec5dbde4a036dd36a0586
ismailGT <89014548+ismailGT@users.noreply.github.com>
2021-09-30 13:51:29 +0200

    Delete ICT V2/ICT V2 directory

--- ICT V2/ICT V2/index.js
+++
@@ -1,9 +0,0 @@
-function read_more()
-{
-    Element = document.querySelector('div.display');
-    Element.classList.remove("display");
-
-    Elementbtn = document.querySelector('a.btn');
-    Elementbtn.remove('btn');
-    // console.log(Element);
-}
