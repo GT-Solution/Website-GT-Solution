a35863801d09cb62d619cfa283c64d0cdf8d1762
Luukgt <92990456+Luukgt@users.noreply.github.com>
2021-10-25 10:48:34 +0200

    Marketing-leadgenerarion
    Subpagina Lead Generation voor Marketing gemaakt.

---
+++ Marketing-leadgeneration/index.js
@@ -0,0 +1,9 @@
+function read_more()
+{
+    Element = document.querySelector('div.display');
+    Element.classList.remove("display");
+
+    Elementbtn = document.querySelector('a.btn');
+    Elementbtn.remove('btn');
+    // console.log(Element);
+}
